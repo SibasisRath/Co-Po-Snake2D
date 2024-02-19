6b71f7f0b6ff30ca8415be075046204fc10e8696
Sibasis Rath <sibasisrath2000@gmail.com>
2024-02-20 03:50:30 +0530

    lobby scene done

--- Co-Po-Snake2D/Assets/ScriptableObject/SoundInfo.cs
+++ Co-Po-Snake2D/Assets/ScriptableObject/SoundInfo.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "ScriptableObject/SoundInfo")]
 public class SoundInfo : ScriptableObject
 {
-    [SerializeField] [Range(0, 1)] private float backGroundSound;
-    [SerializeField] [Range(0, 1)] private float soundEffect;
+    [SerializeField] [Range(0, 1)] private float backGroundSound = 0.5f;
+    [SerializeField] [Range(0, 1)] private float soundEffect = 0.5f;
 
     public float BackGroundSound { get => backGroundSound; set => backGroundSound = value; }
     public float SoundEffect { get => soundEffect; set => soundEffect = value; }
--- Co-Po-Snake2D/Assets/_Scripts/UI/GameModeSelectorUIManager.cs
+++ Co-Po-Snake2D/Assets/_Scripts/UI/GameModeSelectorUIManager.cs
@@ -1,18 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class GameModeSelectorUIManager : MonoBehaviour
 {
+    [Header("panel")]
+    [SerializeField] private GameObject mainPanel;
+    [Space]
+    [Header("Buttons")]
+    [SerializeField] private Button backToMainButton;
+    [SerializeField] private Button singleModeButton;
+    [SerializeField] private Button copoModeButton;
+    [Space]
+    [Header("Serialize Object")]
+    [SerializeField] private GameModeManager modeManager;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainPanel.SetActive(false);
+        SetUpButton(backToMainButton, BackToMainButtonIsClicked);
+        SetUpButton(singleModeButton, SingleModeButtonIsClicked);
+        SetUpButton(copoModeButton, CopoModeButtonIsClicked);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void SetUpButton(Button button, UnityAction unityAction)
+    {
+        if (button != null)
+        {
+            //Sound
+            button.onClick.AddListener(() => { unityAction?.Invoke(); });
+        }
+        else
+        {
+            Debug.Log($"error: {button} is null.");
+        }
+    }
+
+    private void BackToMainButtonIsClicked()
+    {
+        mainPanel.SetActive(true);
+        this.gameObject.SetActive(false);
+    }
+
+    private void SingleModeButtonIsClicked()
+    {
+        modeManager.GameMode = GameModes.SinglePlayer;
+    }
+
+    private void CopoModeButtonIsClicked()
+    {
+        modeManager.GameMode = GameModes.CopoPlayer;
     }
 }
